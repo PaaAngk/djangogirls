ed86cfbdf86a548bc34a26f6c93a09ebe49597c9
Deduch <paaAngarsk@yandex.ru>
2021-06-29 14:54:20 +0800

    full comment

--- blog/models.py
+++ blog/models.py
@@ -33,29 +33,3 @@ class Сomments(models.Model):
     def __str__(self):
         return self.author
         #        return '%s, %s' % (self.author, self.first_name)
-
-    def comment_as_list(self):
-        mylist = []
-        if self.comment == "null":
-            return mylist
-        if len(self.comment) > 0:
-            return list(map(int, self.comment.split()))
-        return mylist
-
-    def get_comment(self):
-        return map(int, self.comment.split())
-
-class SubСomments(models.Model):
-    comment = models.ForeignKey(Сomments, on_delete=models.CASCADE)
-    author = models.CharField(max_length=50)
-    text = models.TextField()
-    published_date = models.DateTimeField(blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.author
-        #        return '%s, %s' % (self.author, self.firsl
-
